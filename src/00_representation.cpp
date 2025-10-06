44ade57fb5737ffe9804155b942dfca84d8a9c27
Lazy Cat <yagi.st123@gmail.com>
2025-10-06 17:53:21 +0700

    chore: add more adjencency matrix examples in 00.cpp

--- src/00_representation.cpp
+++ src/00_representation.cpp
@@ -6,4 +6,10 @@ int main() {
     adj_list.add_edge(2, 3);
 
     adj_list.print();
+
+    Graph<size_t ,true> adj_matrix(10);
+    adj_matrix.add_edge(1, 2);
+    adj_matrix.add_edge(2, 3);
+
+    adj_matrix.print();
 }
